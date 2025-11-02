c14d1440621e4b6f1c1fcb50dfc032689c607885
chishige1217200 <daichicchi01@gmail.com>
2025-11-03 00:46:40 +0900

    Add: お気に入りチェックボックスを追加

--- components/ItemCard.tsx
+++ components/ItemCard.tsx
@@ -2,6 +2,7 @@
 import { useEffect, useState } from "react";
 import { MultiFormatItems } from "@/types/MultiformatItems";
 import { Button } from "@progress/kendo-react-buttons";
+import { Checkbox, CheckboxChangeEvent } from "@progress/kendo-react-inputs";
 import {
   Card,
   CardActions,
@@ -12,12 +13,34 @@ import {
 export default function ItemCard({ id }: { id: string }) {
   const [book, setBook] = useState<MultiFormatItems | null>(null);
   const [error, setError] = useState<string | null>(null);
+  const [checked, setChecked] = useState(false);
 
   useEffect(() => {
     if (id) {
       handleFetch(id);
+      restoreChecked(id);
     }
   }, [id]);
+
+  const restoreChecked = (id: string) => {
+    const stored = localStorage.getItem(`checked-${id}`);
+    if (stored === "1") {
+      setChecked(true);
+    }
+  };
+
+  const onChange = (e: CheckboxChangeEvent) => {
+    const value = e.value;
+    if (id && value != null) {
+      if (value) {
+        localStorage.setItem(`checked-${id}`, "1");
+      } else {
+        localStorage.removeItem(`checked-${id}`);
+      }
+    }
+
+    setChecked(value);
+  };
 
   const handleFetch = async (id: string) => {
     setError(null);
@@ -41,7 +64,7 @@ export default function ItemCard({ id }: { id: string }) {
     }
   };
 
-  function getColor() {
+  const getColor = () => {
     if (error) {
       return "error";
     }
@@ -55,9 +78,9 @@ export default function ItemCard({ id }: { id: string }) {
     }
 
     return undefined;
-  }
+  };
 
-  function getTitle() {
+  const getTitle = () => {
     if (error) {
       return "取得エラー";
     }
@@ -67,41 +90,43 @@ export default function ItemCard({ id }: { id: string }) {
     }
 
     return "取得中...";
-  }
+  };
 
-  function getBody() {
+  const getBody = () => {
     if (error) {
-      return `エラー: ${error}`;
+      return error;
     }
     if (book) {
       return `${book.item.price}円`;
     }
 
     return "取得中...";
-  }
+  };
 
-  function getButtonText() {
+  const getButtonText = () => {
     if (error) {
       return "取得エラー";
     }
     if (book) {
       return book.item.delv_date_label;
-      // return book.item.delv_date_id !== 22 ? "在庫あり" : "在庫なし";
     }
     return "取得中...";
-  }
+  };
 
-  function getUrl() {
+  const getUrl = () => {
     if (book) {
       return `https://books.rakuten.co.jp/rb/${encodeURIComponent(id)}/`;
     }
-  }
+  };
 
   return (
     <>
       <Card style={{ width: 250 }} type={getColor()}>
         <CardBody>
-          <CardTitle>{getTitle()}</CardTitle>
+          <div className="flex justify-between">
+            <CardTitle>{getTitle()}</CardTitle>
+            <Checkbox checked={checked} onChange={onChange} />
+          </div>
           <p>{getBody()}</p>
         </CardBody>
         <CardActions>
